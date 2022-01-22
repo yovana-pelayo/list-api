d31d02bf96cc847c21146758f0b234ec3a00d98f
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-01-21 22:58:36 -0800

    set url to variable and set up the await keywork.. we rendered the page

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 // import functions
+export async function getPokedex() {
+    let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+    const resp = await fetch(url);
+    const json = await resp.json();
+    return json.results;  
+}
+//no idea what json does!!! I NTS: look it up 
+// I understand that return is grabbign the results but from where?
+// set the url to a variable
+// set the response to a variable we used to fetch to get the data from the API
+// we used await to wait for the response to be ready
+//if we do not use await the code will continue to run before the response is ready
+
 
 // grab DOM elements
 
